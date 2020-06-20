822e6c8b1b72088029b8b22c243d6887de0f48cc
rxi <rxi@users.noreply.github.com>
2020-06-20 08:57:53 +0100

    Updated copyright year: 2017 => 2020

--- src/log.h
+++ src/log.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017 rxi
+ * Copyright (c) 2020 rxi
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the MIT license. See `log.c` for details.
